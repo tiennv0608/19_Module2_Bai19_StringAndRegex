b94f4d95867049de332ca1781d2bfdf93750a509
tiennv0608 <tiennv0608@gmail.com>
2021-06-02 09:08:56 +0700

    b4

--- 04_B4_ValidateTencualophoc/src/ValidateClass.java
+++ 04_B4_ValidateTencualophoc/src/ValidateClass.java
@@ -3,9 +3,11 @@ import java.util.regex.Pattern;
 
 public class ValidateClass {
     private static final String CLASS_REGEX = "^[ACP]\\d{4}[GHIKLM]$";
-    public ValidateClass(){
+
+    public ValidateClass() {
     }
-    public boolean validate(String regex){
+
+    public boolean validate(String regex) {
         Pattern pattern = Pattern.compile(CLASS_REGEX);
         Matcher matcher = pattern.matcher(regex);
         return matcher.matches();
