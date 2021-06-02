3137b96af8651e97e4f3d8720773be5621dc9702
tiennv0608 <tiennv0608@gmail.com>
2021-06-02 14:50:33 +0700

    b6

--- 05_B5_ValidatePhone/src/PhoneExample.java
+++ 05_B5_ValidatePhone/src/PhoneExample.java
@@ -2,7 +2,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class PhoneExample {
-    private static final String PHONE_REGEX = "^\\(\\d{2}\\)[-]\\([0]{1}\\d{9}\\)";
+    private static final String PHONE_REGEX = "^\\(\\d{2}\\)[-]\\([0]{1}\\d{9}\\)$";
 
     public PhoneExample() {
     }
--- 06_B6_CrawlTintuc/src/CrawlNews.java
+++ 06_B6_CrawlTintuc/src/CrawlNews.java
@@ -1,30 +1,26 @@
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.UnsupportedEncodingException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class CrawlSongExample {
+public class CrawlNews {
     public static void main(String[] args) {
         try {
-            URL url = new URL("https://www.nhaccuatui.com/bai-hat/nhac-tre-moi.html");
+            URL url = new URL("https://dantri.com.vn/the-gioi.htm");
             Scanner scanner = new Scanner(new InputStreamReader(url.openStream()));
             scanner.useDelimiter("\\Z");
             String content = scanner.next();
             scanner.close();
             content = content.replaceAll("\\n+", "");
-            Pattern p = Pattern.compile("name_song\">(.*?)</a>");
+            Pattern p = Pattern.compile("title=\"(.*?)\"");
             Matcher m = p.matcher(content);
-//            System.out.println(m);
             while (m.find()) {
                 System.out.println(m.group(1));
             }
         } catch (MalformedURLException e) {
-            e.printStackTrace();
-        } catch (UnsupportedEncodingException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
